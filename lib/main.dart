baa072342d90be00ce12ab34900e5960242f319b
Edric <edricrusmana84@gmail.com>
2020-09-22 10:56:56 +0700

    adding clipstream

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:clipstream/Pages/intro_page.dart';
+import 'package:clipstream/screen/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:intro_slider/intro_slider.dart';
+import 'package:intro_slider/slide_object.dart';
+import 'package:intro_slider/dot_animation_enum.dart';
+
+import 'Pages/test.dart';
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Uree',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Rubik',
+        primarySwatch: Colors.deepPurple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: CameraScreen(),
+    );
+  }
+}
+
+void main() => runApp(new MyApp());
+
---
+++ lib/screen/stack.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CameraScreenTest extends StatefulWidget {
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreenTest> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
